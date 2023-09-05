8cf1a42c06d97d6fcc67993c9ba2104e805bf9a5
MatsHalvorsen <82614331+MatsHalvorsen@users.noreply.github.com>
2023-09-05 13:01:22 +0200

    added a randomNumber function for x and y axis

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 //model
-
-
+var divElement = document.getElementById('x');
 
 //view
 updateView();
 
 function updateView() {
     document.getElementById('app').innerHTML = /*HTML*/ `
-
+    
     `;
 }
 
 
 //controller
-function changeColor(x) {
+function changeColor(x, divElement) {
     x.style.color = 'rgb(0, 250, 250)';
     x.style.cursor = 'crosshair';
+    divElement.style.left = `${randomNumber()}`;
+    divElement.style.top = `${randomNumber()}`;
     //x.style.backgroundColor = "blue";
 
 }
@@ -23,3 +24,9 @@ function changeColor(x) {
 function changeBack(x) {
     x.style.color = 'rgb(250, 250, 250)';
 }
+
+function randomNumber() {
+    return Math.floor(Math.random() * 1001);
+}
+
+console.log(randomNumber());
